a6676d2a72741a80d5dd6eb79b20383798d0dec5
MatthewSheppard1 <matthewsheppard02@gmail.com>
2024-12-16 09:48:54 -0700

    Edited decode_mask function to work with batches

--- encoder_decoder.py
+++ encoder_decoder.py
@@ -1,5 +1,5 @@
 import chess
-import numpy as np
+import torch
 
 # Layers:
 # 0-11 piece representation
@@ -11,7 +11,7 @@ import numpy as np
 
 #Encode the board from chess.Board representation to NN input representation
 def encode_board(board: chess.Board):
-    encoded = np.zeros([12, 8, 8])
+    encoded = torch.zeros([12, 8, 8])
 
     for square in chess.SQUARES:
         piece = board.piece_at(square)
@@ -27,7 +27,7 @@ def encode_board(board: chess.Board):
 
 #TODO: idk if i'll actually need this, so i'll finish if I find a need for it
 def decode_board(encoded) -> chess.Board:
-    decoded = np.zeros([64])
+    decoded = torch.zeros([64])
 
     for i in range(len(encoded)):
         for j in range(len(encoded[0])):
@@ -44,7 +44,7 @@ def get_move_mask_index(dist, direction):
 
 
 def make_move_mask(board: chess.Board):
-    mask = np.zeros([8, 8, 64]).astype(int)
+    mask = torch.zeros([8, 8, 64])
 
     vals = "abcdefgh"
     for move in board.legal_moves:
@@ -91,64 +91,163 @@ def make_move_mask(board: chess.Board):
                 direction = 6
 
             mask[coords[2], coords[0], get_move_mask_index(dist, direction)] = 1
-    return mask
+    return mask.view([-1])
 
+def encode_move(move: chess.Move):
+    move_t = torch.zeros([8, 8])
+    move_t[int(move.from_square / 8)][move.from_square % 8] = 1
+    move_t[int(move.to_square / 8)][move.to_square % 8] = 1
 
-def decode_mask(mask) -> list[(chess.Move, float)]:
-    moves = []
-
+    return move_t
+def decode_move(move_t):
     vals = "abcdefgh"
-    for i in range(len(mask)):
-        for j in range(len(mask[0])):
-            for k in range(len(mask[0][0])):
-                coords = [i, j + 1, 0, 0]
-                if(mask[i][j][k] > 0):
-                    if(k > 55):
-                        direction = k - 56
+    coords = [0, 0, 0, 0]
+    for i in range(len(move_t)):
+        for j in range(len(move_t[0])):
+            if(move_t[i][j] == 1):
+                coords[0] = i
+                coords[1] = j + 1
+            if (move_t[i][j] == 2):
+                coords[2] = i
+                coords[3] = j + 1
+    return chess.Move.from_uci(vals[coords[0]] + str(coords[1]) + vals[coords[2]] + str(coords[3]))
 
-                        if (direction == 0):
-                            coords[2] = coords[0] + 1
-                            coords[3] = coords[1] + 2
-                        elif (direction == 1):
-                            coords[2] = coords[0] + 2
-                            coords[3] = coords[1] + 1
-                        elif (direction == 2):
-                            coords[2] = coords[0] + 2
-                            coords[3] = coords[1] - 1
-                        elif (direction == 3):
-                            coords[2] = coords[0] + 1
-                            coords[3] = coords[1] - 2
-                        elif (direction == 4):
-                            coords[2] = coords[0] - 1
-                            coords[3] = coords[1] - 2
-                        elif (direction == 5):
-                            coords[2] = coords[0] - 2
-                            coords[3] = coords[1] - 1
-                        elif (direction == 6):
-                            coords[2] = coords[0] - 2
-                            coords[3] = coords[1] + 1
-                        elif (direction == 7):
-                            coords[2] = coords[0] - 1
-                            coords[3] = coords[1] + 2
 
-                    else:
-                        direction = int(k / 7)
-                        dist = (k % 7) + 1
-                        mods = [0, 0]
 
-                        if (direction in [0, 1, 7]):
-                            mods[1] = 1
-                        if (direction in [1, 2, 3]):
-                            mods[0] = 1
-                        if (direction in [3, 4, 5]):
-                            mods[1] = -1
-                        if (direction in [5, 6, 7]):
-                            mods[0] = -1
 
-                        coords[2] = coords[0] + (mods[0] * dist)
-                        coords[3] = coords[1] + (mods[1] * dist)
+def decode_mask(mask, board: chess.Board, batch_size=None):
+    if(batch_size is None):
+        moves = []
+        probs = []
+        mask = mask.view([8, 8, 64])
+        vals = "abcdefgh"
+        for i in range(len(mask)):
+            for j in range(len(mask[0])):
+                for k in range(len(mask[0][0])):
+                    coords = [i, j + 1, 0, 0]
+                    if(mask[i][j][k] != 0):
+                        if(k > 55):
+                            direction = k - 56
 
-                    move_uci = vals[coords[0]] + str(coords[1]) + vals[coords[2]] + str(coords[3])
-                    moves.append((chess.Move.from_uci(move_uci), mask[i][j][k]))
+                            if (direction == 0):
+                                coords[2] = coords[0] + 1
+                                coords[3] = coords[1] + 2
+                            elif (direction == 1):
+                                coords[2] = coords[0] + 2
+                                coords[3] = coords[1] + 1
+                            elif (direction == 2):
+                                coords[2] = coords[0] + 2
+                                coords[3] = coords[1] - 1
+                            elif (direction == 3):
+                                coords[2] = coords[0] + 1
+                                coords[3] = coords[1] - 2
+                            elif (direction == 4):
+                                coords[2] = coords[0] - 1
+                                coords[3] = coords[1] - 2
+                            elif (direction == 5):
+                                coords[2] = coords[0] - 2
+                                coords[3] = coords[1] - 1
+                            elif (direction == 6):
+                                coords[2] = coords[0] - 2
+                                coords[3] = coords[1] + 1
+                            elif (direction == 7):
+                                coords[2] = coords[0] - 1
+                                coords[3] = coords[1] + 2
 
-    return moves
+                        else:
+                            direction = int(k / 7)
+                            dist = (k % 7) + 1
+                            mods = [0, 0]
+
+                            if (direction in [0, 1, 7]):
+                                mods[1] = 1
+                            if (direction in [1, 2, 3]):
+                                mods[0] = 1
+                            if (direction in [3, 4, 5]):
+                                mods[1] = -1
+                            if (direction in [5, 6, 7]):
+                                mods[0] = -1
+
+                            coords[2] = coords[0] + (mods[0] * dist)
+                            coords[3] = coords[1] + (mods[1] * dist)
+
+                        move_uci = vals[coords[0]] + str(coords[1]) + vals[coords[2]] + str(coords[3])
+                        if((coords[3] == 1 or coords[3] == 8) and board.piece_type_at(chess.parse_square(vals[coords[0]] + str(coords[1]))) == 1):
+                            move_uci += "q"
+
+                        moves.append(chess.Move.from_uci(move_uci))
+                        probs.append(mask[i][j][k])
+        probs = torch.tensor(probs)
+        return moves, probs
+    else:
+        mask = mask.view([batch_size, 8, 8, 64])
+        vals = "abcdefgh"
+        batch_probs = []
+        batch_moves = []
+        for b in range(batch_size):
+            moves = []
+            probs = []
+            for i in range(len(mask)):
+                for j in range(len(mask[0])):
+                    for k in range(len(mask[0][0])):
+                        coords = [i, j + 1, 0, 0]
+                        if (mask[b][i][j][k] != 0):
+                            if (k > 55):
+                                direction = k - 56
+
+                                if (direction == 0):
+                                    coords[2] = coords[0] + 1
+                                    coords[3] = coords[1] + 2
+                                elif (direction == 1):
+                                    coords[2] = coords[0] + 2
+                                    coords[3] = coords[1] + 1
+                                elif (direction == 2):
+                                    coords[2] = coords[0] + 2
+                                    coords[3] = coords[1] - 1
+                                elif (direction == 3):
+                                    coords[2] = coords[0] + 1
+                                    coords[3] = coords[1] - 2
+                                elif (direction == 4):
+                                    coords[2] = coords[0] - 1
+                                    coords[3] = coords[1] - 2
+                                elif (direction == 5):
+                                    coords[2] = coords[0] - 2
+                                    coords[3] = coords[1] - 1
+                                elif (direction == 6):
+                                    coords[2] = coords[0] - 2
+                                    coords[3] = coords[1] + 1
+                                elif (direction == 7):
+                                    coords[2] = coords[0] - 1
+                                    coords[3] = coords[1] + 2
+
+                            else:
+                                direction = int(k / 7)
+                                dist = (k % 7) + 1
+                                mods = [0, 0]
+
+                                if (direction in [0, 1, 7]):
+                                    mods[1] = 1
+                                if (direction in [1, 2, 3]):
+                                    mods[0] = 1
+                                if (direction in [3, 4, 5]):
+                                    mods[1] = -1
+                                if (direction in [5, 6, 7]):
+                                    mods[0] = -1
+
+                                coords[2] = coords[0] + (mods[0] * dist)
+                                coords[3] = coords[1] + (mods[1] * dist)
+
+                            print(b, i, j, k)
+
+                            move_uci = vals[coords[0]] + str(coords[1]) + vals[coords[2]] + str(coords[3])
+                            if ((coords[3] == 1 or coords[3] == 8) and board.piece_type_at(
+                                    chess.parse_square(vals[coords[0]] + str(coords[1]))) == 1):
+                                move_uci += "q"
+
+                            moves.append(chess.Move.from_uci(move_uci))
+                            probs.append(mask[b][i][j][k])
+
+            probs = torch.tensor(probs)
+            batch_probs.append(probs)
+            batch_moves.append(moves)
+        return batch_moves, batch_probs
